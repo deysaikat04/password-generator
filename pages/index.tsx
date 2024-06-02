111e4f3b251917889868ae70296b09c137b4fdc7
Saikat Dey <30950554+deysaikat04@users.noreply.github.com>
2024-06-02 11:30:08 +0530

    Base setup for password generator added

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,118 +1,148 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
+import { Input } from "@/components/ui/input";
+import { Slider } from "@/components/ui/slider";
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Label } from "@/components/ui/label";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { useState } from "react";
+import { cn } from "@/lib/utils";
+import { Switch } from "@/components/ui/switch";
+import { RotateCw, Copy } from "lucide-react";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
+  const [length, setLength] = useState(20);
+
+
   return (
     <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
+      className={`flex min-h-screen flex-col items-center p-4 sm:p-2 bg-[#314455]`}
     >
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">pages/index.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+      <div className="font-semibold text-2xl sm:text-5xl text-white mb-2 mt-24">
+        <h1>Password Generator</h1>
+      </div>
+
+      <div className="w-full grid grid-cols-12 p-4 mt-6 rounded-lg ">
+        <div className="col-span-12 sm:col-span-8 sm:col-start-3 ">
+          <Input
+            type="text"
+            placeholder="Email"
+            className={cn(
+              "h-[60px] rounded-[50px] text-center text-xl sm:text-2xl font-medium bg-white border-[#66fcf1] border"
+            )}
+            value={"Qsd@324s@#sd23s@##$#"}
+          />
         </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      <div className=" col-span-12 mt-2">
+        <div className="grid grid-cols-12 gap-4 mb-6">
+          <div className=" col-span-6">
+            <Button className="w-full">
+              <RotateCw className="animate-refresh" />
+            </Button>
+          </div>
+          <div className=" col-span-6">
+            <Button className="w-full bg-[#f79e02] hover:bg-[#eb190e]">
+              <Copy />
+            </Button>
+          </div>
+        </div>
       </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+      <Card className="sm:w-[60%]">
+        <div className="p-1 justify-center text-center rounded-md">
+          <CardContent>
+            <div className="grid grid-cols-12 p-4 mt-6  rounded-lg">
+              <div className="col-span-12 mt-2">
+                <div className="grid grid-cols-12 gap-10">
+                  <div className="col-span-8 sm:col-span-8 ">
+                    <div className="col-span-8 sm:col-span-6 flex m-4">
+                      <Label className="mr-2">Length</Label>
+                      <Slider
+                        defaultValue={[length]}
+                        max={100}
+                        min={6}
+                        step={1}
+                        className={cn("cursor-pointer m-2 w-full sm:w-[100%]")}
+                        onValueChange={(event) => setLength(event[0])}
+                      />
+                      {length}
+                    </div>
+                    <div className="col-span-8 sm:col-span-6 flex m-4">
+                      <Select defaultValue="randomPassword">
+                        <SelectTrigger className="w-full">
+                          <SelectValue placeholder="Select a fruit" />
+                        </SelectTrigger>
+                        <SelectContent>
+                          <SelectGroup>
+                            <SelectItem value="randomPassword">
+                              Random Password
+                            </SelectItem>
+                            <SelectItem value="pin">PIN</SelectItem>
+                          </SelectGroup>
+                        </SelectContent>
+                      </Select>
+                    </div>
+                  </div>
+                  <div className=" col-span-8 sm:col-span-4 mt-2">
+                    <div className="grid grid-cols-6 gap-4 content-center">
+                      <div className="col-span-3 sm:col-span-12">
+                        <div className="content-center">
+                          <div className="flex items-center justify-start space-x-4 align-middle">
+                            <Switch id="airplane-mode" className="bg-[red]" />
+                            <Label htmlFor="airplane-mode">Uppercase</Label>
+                          </div>
+                        </div>
+                      </div>
+                      <div className="col-span-3 sm:col-span-12">
+                        <div className="content-center">
+                          <div className="flex items-center justify-start space-x-4 align-middle">
+                            <Switch id="airplane-mode" />
+                            <Label htmlFor="airplane-mode">Lowercase</Label>
+                          </div>
+                        </div>
+                      </div>
+                      <div className="col-span-3 sm:col-span-12">
+                        <div className="content-center">
+                          <div className="flex items-center justify-start space-x-4 align-middle">
+                            <Switch id="airplane-mode" />
+                            <Label htmlFor="airplane-mode">Symbols</Label>
+                          </div>
+                        </div>
+                      </div>
+                      <div className="col-span-3 sm:col-span-12">
+                        <div className="content-center">
+                          <div className="flex items-center justify-start space-x-4 align-middle">
+                            <Switch id="airplane-mode" />
+                            <Label htmlFor="airplane-mode">Numbers</Label>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </CardContent>
+        </div>
+      </Card>
     </main>
   );
 }
