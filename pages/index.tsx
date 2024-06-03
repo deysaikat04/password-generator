11b9bd7099c4d1a013fe133f624e2324037ea4aa
Saikat Dey <30950554+deysaikat04@users.noreply.github.com>
2024-06-03 12:08:41 +0530

    Random password parameters updated

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,14 +3,7 @@ import { Inter } from "next/font/google";
 import { Input } from "@/components/ui/input";
 import { Slider } from "@/components/ui/slider";
 import { Button } from "@/components/ui/button";
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
+import { Card, CardContent } from "@/components/ui/card";
 import { Label } from "@/components/ui/label";
 import {
   Select,
@@ -21,127 +14,253 @@ import {
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select";
-import { useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { cn } from "@/lib/utils";
 import { Switch } from "@/components/ui/switch";
 import { RotateCw, Copy } from "lucide-react";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export default function Home() {
-  const [length, setLength] = useState(20);
+enum PasswordTypeEnum {
+  RandomPassword = "randomPassword",
+  Pin = "pin",
+}
 
+export default function Home() {
+  const [password, setPassword] = useState("");
+
+  const [length, setLength] = useState(16);
+  const [maxLength, setMaxLength] = useState(100);
+  const [passwordType, setPasswordType] = useState(
+    PasswordTypeEnum.RandomPassword
+  );
+  const [isUpperCaseChecked, setIsUpperCaseChecked] = useState(false);
+  const [isLowerCaseChecked, setIsLowerCaseChecked] = useState(true);
+  const [isSymbolChecked, setIsSymbolChecked] = useState(false);
+  const [isNumberChecked, setIsNumberChecked] = useState(false);
+
+  const upperCaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  const lowerCaseChars = "abcdefghijklmnopqrstuvwxyz";
+  const numberChars = "0123456789";
+  const symbolChars = "!@#$%^&*()_+[]{}|;:,.<>?";
+
+  const isPinSelected = passwordType === PasswordTypeEnum.Pin;
+
+  const generateRandomPassword = () => {
+    let allChars = "";
+    let password = "";
+
+    if (passwordType === PasswordTypeEnum.RandomPassword) {
+      if (isUpperCaseChecked) allChars += upperCaseChars;
+      if (isLowerCaseChecked) allChars += lowerCaseChars;
+      if (isNumberChecked) allChars += numberChars;
+      if (isSymbolChecked) allChars += symbolChars;
+
+      for (let i = 0; i < length; i++) {
+        const randomIndex = Math.floor(Math.random() * allChars.length);
+        password += allChars[randomIndex];
+      }
+    } else {
+      if (isNumberChecked) allChars += numberChars;
+
+      for (let i = 0; i < length; i++) {
+        const randomIndex = Math.floor(Math.random() * allChars.length);
+        password += allChars[randomIndex];
+      }
+    }
+
+    setPassword(password);
+  };
+
+  useEffect(() => {
+    generateRandomPassword();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [
+    isUpperCaseChecked,
+    isLowerCaseChecked,
+    isSymbolChecked,
+    isNumberChecked,
+    length,
+  ]);
+
+  useEffect(() => {
+    if (
+      !isUpperCaseChecked &&
+      !isLowerCaseChecked &&
+      !isSymbolChecked &&
+      !isNumberChecked
+    ) {
+      setIsLowerCaseChecked(true);
+    }
+  }, [
+    isUpperCaseChecked,
+    isLowerCaseChecked,
+    isSymbolChecked,
+    isNumberChecked,
+  ]);
+
+  useEffect(() => {
+    if (passwordType === PasswordTypeEnum.Pin) {
+      setIsNumberChecked(true);
+      setIsUpperCaseChecked(false);
+      setIsLowerCaseChecked(false);
+      setIsSymbolChecked(false);
+      setMaxLength(20);
+      setLength(4);
+    } else {
+      setMaxLength(100);
+      setLength(16);
+      setIsNumberChecked(false);
+      setIsLowerCaseChecked(true);
+    }
+  }, [passwordType]);
+
+  async function copyContent() {
+    try {
+      await navigator.clipboard.writeText(password);
+      console.log("Content copied to clipboard");
+      /* Resolved - text copied to clipboard successfully */
+    } catch (err) {
+      console.error("Failed to copy: ", err);
+      /* Rejected - text failed to copy to the clipboard */
+    }
+  }
 
   return (
     <main
       className={`flex min-h-screen flex-col items-center p-4 sm:p-2 bg-[#314455]`}
     >
-      <div className="font-semibold text-2xl sm:text-5xl text-white mb-2 mt-24">
+      <div className="font-semibold text-2xl sm:text-4xl text-white mb-2 mt-24">
         <h1>Password Generator</h1>
       </div>
 
       <div className="w-full grid grid-cols-12 p-4 mt-6 rounded-lg ">
-        <div className="col-span-12 sm:col-span-8 sm:col-start-3 ">
+        <div className="col-span-12 sm:col-span-6 sm:col-start-4 ">
           <Input
             type="text"
-            placeholder="Email"
             className={cn(
               "h-[60px] rounded-[50px] text-center text-xl sm:text-2xl font-medium bg-white border-[#66fcf1] border"
             )}
-            value={"Qsd@324s@#sd23s@##$#"}
+            value={password}
           />
         </div>
       </div>
-      <div className=" col-span-12 mt-2">
+
+      {/* Buttons start */}
+      <div className="col-span-12 mt-2">
         <div className="grid grid-cols-12 gap-4 mb-6">
           <div className=" col-span-6">
-            <Button className="w-full">
+            <Button className="w-full" onClick={generateRandomPassword}>
               <RotateCw className="animate-refresh" />
             </Button>
           </div>
           <div className=" col-span-6">
-            <Button className="w-full bg-[#f79e02] hover:bg-[#eb190e]">
+            <Button
+              className="w-full bg-[#f79e02] hover:bg-[#e0940f]"
+              onClick={copyContent}
+            >
               <Copy />
             </Button>
           </div>
         </div>
       </div>
-      <Card className="sm:w-[60%]">
-        <div className="p-1 justify-center text-center rounded-md">
-          <CardContent>
-            <div className="grid grid-cols-12 p-4 mt-6  rounded-lg">
-              <div className="col-span-12 mt-2">
-                <div className="grid grid-cols-12 gap-10">
-                  <div className="col-span-8 sm:col-span-8 ">
-                    <div className="col-span-8 sm:col-span-6 flex m-4">
-                      <Label className="mr-2">Length</Label>
-                      <Slider
-                        defaultValue={[length]}
-                        max={100}
-                        min={6}
-                        step={1}
-                        className={cn("cursor-pointer m-2 w-full sm:w-[100%]")}
-                        onValueChange={(event) => setLength(event[0])}
-                      />
-                      {length}
-                    </div>
-                    <div className="col-span-8 sm:col-span-6 flex m-4">
-                      <Select defaultValue="randomPassword">
-                        <SelectTrigger className="w-full">
-                          <SelectValue placeholder="Select a fruit" />
-                        </SelectTrigger>
-                        <SelectContent>
-                          <SelectGroup>
-                            <SelectItem value="randomPassword">
-                              Random Password
-                            </SelectItem>
-                            <SelectItem value="pin">PIN</SelectItem>
-                          </SelectGroup>
-                        </SelectContent>
-                      </Select>
-                    </div>
+      {/* Buttons end */}
+
+      <Card className="md:w-[50%]">
+        <CardContent
+          className={cn("p-1 justify-center text-center rounded-md")}
+        >
+          <div className="grid grid-cols-12 p-4 rounded-lg gap-2 sm:gap-4">
+            <div className="col-span-12 sm:col-span-8 mt-2 ">
+              <div className="flex flex-col">
+                <div className="flex m-4">
+                  <Label className="mr-2">Length</Label>
+                  <Slider
+                    defaultValue={[length]}
+                    max={maxLength}
+                    min={6}
+                    step={1}
+                    className={cn("cursor-pointer m-2 w-full sm:w-[100%]")}
+                    onValueChange={(event) => setLength(event[0])}
+                  />
+                  {length}
+                </div>
+                <div>
+                  <Select
+                    defaultValue="randomPassword"
+                    value={passwordType}
+                    onValueChange={(value) =>
+                      setPasswordType(value as PasswordTypeEnum)
+                    }
+                  >
+                    <SelectTrigger className="w-full">
+                      <SelectValue placeholder="Select a fruit" />
+                    </SelectTrigger>
+                    <SelectContent>
+                      <SelectGroup>
+                        <SelectItem value={PasswordTypeEnum.RandomPassword}>
+                          Random Password
+                        </SelectItem>
+                        <SelectItem value={PasswordTypeEnum.Pin}>
+                          PIN
+                        </SelectItem>
+                      </SelectGroup>
+                    </SelectContent>
+                  </Select>
+                </div>
+              </div>
+            </div>
+
+            <div className="col-span-12 sm:col-span-4 mt-2">
+              <div className="flex flex-row  flex-wrap sm:flex-col">
+                <div className="content-center p-2">
+                  <div className="flex items-center justify-start space-x-4 align-middle">
+                    <Switch
+                      id="airplane-mode"
+                      checked={isUpperCaseChecked}
+                      onCheckedChange={setIsUpperCaseChecked}
+                      disabled={isPinSelected}
+                    />
+                    <Label htmlFor="airplane-mode">Uppercase</Label>
                   </div>
-                  <div className=" col-span-8 sm:col-span-4 mt-2">
-                    <div className="grid grid-cols-6 gap-4 content-center">
-                      <div className="col-span-3 sm:col-span-12">
-                        <div className="content-center">
-                          <div className="flex items-center justify-start space-x-4 align-middle">
-                            <Switch id="airplane-mode" className="bg-[red]" />
-                            <Label htmlFor="airplane-mode">Uppercase</Label>
-                          </div>
-                        </div>
-                      </div>
-                      <div className="col-span-3 sm:col-span-12">
-                        <div className="content-center">
-                          <div className="flex items-center justify-start space-x-4 align-middle">
-                            <Switch id="airplane-mode" />
-                            <Label htmlFor="airplane-mode">Lowercase</Label>
-                          </div>
-                        </div>
-                      </div>
-                      <div className="col-span-3 sm:col-span-12">
-                        <div className="content-center">
-                          <div className="flex items-center justify-start space-x-4 align-middle">
-                            <Switch id="airplane-mode" />
-                            <Label htmlFor="airplane-mode">Symbols</Label>
-                          </div>
-                        </div>
-                      </div>
-                      <div className="col-span-3 sm:col-span-12">
-                        <div className="content-center">
-                          <div className="flex items-center justify-start space-x-4 align-middle">
-                            <Switch id="airplane-mode" />
-                            <Label htmlFor="airplane-mode">Numbers</Label>
-                          </div>
-                        </div>
-                      </div>
-                    </div>
+                </div>
+                <div className="content-center p-2">
+                  <div className="flex items-center justify-start space-x-4 align-middle">
+                    <Switch
+                      id="airplane-mode"
+                      checked={isLowerCaseChecked}
+                      onCheckedChange={setIsLowerCaseChecked}
+                      disabled={isPinSelected}
+                    />
+                    <Label htmlFor="airplane-mode">Lowercase</Label>
+                  </div>
+                </div>
+                <div className="content-center p-2">
+                  <div className="flex items-center justify-start space-x-4 align-middle">
+                    <Switch
+                      id="airplane-mode"
+                      checked={isSymbolChecked}
+                      onCheckedChange={setIsSymbolChecked}
+                      disabled={isPinSelected}
+                    />
+                    <Label htmlFor="airplane-mode">Symbols</Label>
+                  </div>
+                </div>
+                <div className="content-center p-2">
+                  <div className="flex items-center justify-start space-x-4 align-middle">
+                    <Switch
+                      id="airplane-mode"
+                      checked={isPinSelected || isNumberChecked}
+                      onCheckedChange={setIsNumberChecked}
+                      disabled={isPinSelected}
+                    />
+                    <Label htmlFor="airplane-mode">Numbers</Label>
                   </div>
                 </div>
               </div>
             </div>
-          </CardContent>
-        </div>
+          </div>
+        </CardContent>
       </Card>
     </main>
   );
